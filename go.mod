40fa6aff270ad41fd352017d3115c9f53d6e98f4
Peter Davies <pgd@digitalacorn.co.uk>
2023-12-02 16:38:54 +0000

    feat: part 2 - need to find words from the end

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module adventofcode2023
 
 go 1.21.4
+
+require robpike.io/filter v0.0.0-20210831053821-dcb4225e6ac8 // indirect
--- main.go
+++ main.go
@@ -13,6 +13,14 @@ func check(e error) {
 	}
 }
 
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) != 1 {
@@ -26,19 +34,42 @@ func main() {
 	sum := 0
 	fmt.Println("Read", len(lines), "lines")
 
+	digitNames := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	replacerArgs := make([]string, len(digitNames)*2)
+	replacerArgsReverse := make([]string, len(digitNames)*2)
+
+	for i, v := range digitNames {
+		replacerArgs[i*2] = v
+		replacerArgsReverse[i*2] = Reverse(v)
+		replacerArgs[i*2+1] = fmt.Sprintf("%d", i+1)
+		replacerArgsReverse[i*2+1] = fmt.Sprintf("%d", i+1)
+	}
+
+	wordReplacer := strings.NewReplacer(replacerArgs...)
+	wordReplacerReverse := strings.NewReplacer(replacerArgsReverse...)
+
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
-		numerics := regexp.MustCompile("[0-9]").FindAllString(line, -1)
+		forwardLine := wordReplacer.Replace(line)
+		reverseLine := wordReplacerReverse.Replace(Reverse(line))
+		numerics := regexp.MustCompile("[1-9]").FindAllString(forwardLine, -1)
+		numericsReverse := regexp.MustCompile("[1-9]").FindAllString(reverseLine, -1)
+
 		if len(numerics) < 1 {
 			panic("no numbers found")
 		}
+		if len(numericsReverse) < 1 {
+			panic("no numbers found")
+		}
 		first := []byte(numerics[0])[0]
-		last := []byte(numerics[len(numerics)-1])[0]
+		last := []byte(numericsReverse[0])[0]
 		zero := []byte("0")[0]
 		calibrationValue := (first-zero)*10 + last - zero
+
+		fmt.Println("calibrationValue: ", calibrationValue, line)
+
 		sum += int(calibrationValue)
 	}
 
 	fmt.Println("Total ", sum)
-
 }
