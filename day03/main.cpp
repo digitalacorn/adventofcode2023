7a059efbd3cf92cc2bc4ba142372921265a3c317
Peter Davies <pgd@digitalacorn.co.uk>
2023-12-03 18:50:11 +0000

    feat: refactor for part 2

--- day03/main.cpp
+++ day03/main.cpp
@@ -2,9 +2,13 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <vector>
+
 
 #define IS_NUM(c) ((c)>='0' && (c)<='9')
 #define IS_BLANK(c) ((c)=='.')
+#define IS_GEAR(c) ((c)=='*')
 #define IS_SYMBOL(c) (!IS_NUM(c) && !IS_BLANK(c))
 
 class Engine
@@ -14,8 +18,11 @@ private:
     size_t fileLength;
     size_t engineWidth;
     size_t engineHeight;
+    std::map<uint32_t, std::vector<uint32_t> > gears;
     char CharAt(size_t x, size_t y);
     uint32_t ValidateNum(int32_t x1, int32_t x2, int32_t y);
+    void AddToGearAt(int32_t x, int32_t y, uint32_t num);
+    bool ConsiderBorderPoint(int32_t x, int32_t y, uint32_t num);
 public:
     Engine(std::string fileName);
     ~Engine();
@@ -49,27 +56,39 @@ char Engine::CharAt(size_t x, size_t y) {
     return this->buffer[y*(this->engineWidth+1)+x];
 }
 
+void Engine::AddToGearAt(int32_t x, int32_t y, uint32_t num) {
+    uint32_t gearId = y*this->engineWidth+x;
+    if (this->gears.find(gearId) == this->gears.end()) {
+        std::vector<uint32_t> firstEntry;
+        this->gears[gearId] = firstEntry;
+    }
+    this->gears[gearId].push_back(num);
+}
+
+bool Engine::ConsiderBorderPoint(int32_t x, int32_t y, uint32_t num) {
+    char c = this->CharAt(x,y);
+    bool isValid = IS_SYMBOL(c);
+    if (isValid && IS_GEAR(c)) {
+        this->AddToGearAt(x,y, num);
+    }
+    return isValid;
+}
+
 uint32_t Engine::ValidateNum(int32_t x1, int32_t x2, int32_t y) {
     bool isValid = false;
-    for (int32_t i = x1-1; i <= x2+1 ; i++)
-    {
-        char above = this->CharAt(i,y-1);
-        char below = this->CharAt(i,y+1);
-        if (IS_SYMBOL(above) || IS_SYMBOL(below)) {
-            isValid = true;
-            break;
-        }
-    }
-    char left = this->CharAt(x1-1,y);
-    char right = this->CharAt(x2+1,y);
-    if (IS_SYMBOL(left) || IS_SYMBOL(right)) {
-        isValid=true;
-    }
-    if (!isValid) return 0;
+
     uint32_t num = 0;
     for (int32_t i = x1; i <= x2 ; i++) {
         num = num*10 + (this->CharAt(i,y)-'0');
     }
+    for (int32_t i = x1-1; i <= x2+1 ; i++)
+    {
+        isValid = this->ConsiderBorderPoint(i,y-1, num) || isValid;
+        isValid = this->ConsiderBorderPoint(i,y+1, num) || isValid;
+    }
+    isValid = this->ConsiderBorderPoint(x1-1,y, num) || isValid;
+    isValid = this->ConsiderBorderPoint(x2+1,y, num) || isValid;
+    if (!isValid) return 0;
     return num;
 }
 
@@ -102,8 +121,18 @@ int Engine::Examine() {
             }
         }
     }
+
+    u_int32_t totalGearRatio = 0;
+    std::map<uint32_t, std::vector<uint32_t> >::iterator it;
+
+    for (it = this->gears.begin(); it != this->gears.end(); it++) {
+        if (it->second.size() == 2) {
+            totalGearRatio += it->second[0] * it->second[1];
+        }
+    }
     
     printf("\nSum: %d\n", total);
+    printf("\ntotalGearRatio: %d\n", totalGearRatio);
     return 0;
 }
 
