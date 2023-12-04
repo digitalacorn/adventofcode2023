4d965de8abcb34dd943b68224bbb344336da52d3
Peter Davies <pgd@digitalacorn.co.uk>
2023-12-04 21:08:49 +0000

    feat: part2

--- day04/main.lua
+++ day04/main.lua
@@ -13,11 +13,20 @@ file:close()
 
 local totalPoints = 0
 
+local matchCounts = {}
+local cardInstances = {}
+
 for line in content:gmatch("[^\r\n]+") do
     local points = 0
+    local matches = 0
     local winners = {}
     local lineIter = line:gmatch("[^|:]+")
     local cardNumber = tonumber(lineIter():match("%d+"))
+    if cardNumber == nil then
+        os.exit(1)
+    end
+
+    cardInstances[cardNumber] = 1
     local winnersIter = lineIter():gmatch("%d+")
     local numbersIter = lineIter():gmatch("%d+")
     for winnerStr in winnersIter do
@@ -26,6 +35,7 @@ for line in content:gmatch("[^\r\n]+") do
     for numberStr in numbersIter do
         local number = tonumber(numberStr)
         if winners[number] ~= nil then
+            matches = matches + 1
             if points == 0 then
                 points = 1
             else
@@ -33,10 +43,23 @@ for line in content:gmatch("[^\r\n]+") do
             end
         end
     end
+
+    matchCounts[cardNumber] = matches
+
     printf("Card: %d %d \n", cardNumber, points)
 
     totalPoints = totalPoints + points
 end
 
+
 printf("totalPoints: %d \n", totalPoints)
 
+local totalCards = 0
+for card, count in ipairs(cardInstances) do
+    totalCards = totalCards + count
+    for i = card+1, card+matchCounts[card], 1 do
+        cardInstances[i] = cardInstances[i]+count
+    end
+end
+
+printf("totalCards: %d \n", totalCards)
